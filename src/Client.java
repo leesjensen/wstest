558f22265e98afa0cd73d5d84f2353f897b7c195
Lee <lee.jensen@agilix.com>
2023-12-05 10:28:05 -0700

    initial

---
+++ src/Client.java
@@ -0,0 +1,35 @@
+import javax.websocket.*;
+import java.net.URI;
+import java.util.Scanner;
+
+public class Client extends Endpoint {
+
+    public static void main(String[] args) throws Exception {
+        var ws = new Client();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a message you want to echo");
+        while (true) ws.send(scanner.nextLine());
+    }
+
+    public Session session;
+
+    public Client() throws Exception {
+        URI uri = new URI("ws://localhost:8080/connect");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public void send(String msg) throws Exception {
+        this.session.getBasicRemote().sendText(msg);
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+}
